4e4d19a09c56ee32acdfd2eee4e3d88612a86084
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-06-10 16:41:20 +0700

    Initial Commit

---
+++ lib/home-page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:fan_carousel_image_slider/fan_carousel_image_slider.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "My Gallery",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: const Color(0x00000fff),
+        elevation: 0,
+      ),
+      body: Container(
+        child: const TabBar(
+          tabs: [
+            Tab(text: "Nature",),
+            Tab(text: "Street",),
+            Tab(text: "Architecture",)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
